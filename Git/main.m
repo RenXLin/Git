b6b96a5992405f35773e617e130eba1ae7d018de
RenXLin <renxlin@renxlindeMac-Pro.local>
2014-06-04 12:44:23 +0800

    Initial Commit

---
+++ Git/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Git
+//
+//  Created by renxlin on 14-6-4.
+//  Copyright (c) 2014年 任小林. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
